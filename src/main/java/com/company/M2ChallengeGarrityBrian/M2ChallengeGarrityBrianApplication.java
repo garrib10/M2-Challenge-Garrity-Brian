775d079d0b465b02c150da2fabc1b805ae29f81d
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2021-06-07 21:12:30 -0400

    first commit

---
+++ src/main/java/com/company/M2ChallengeGarrityBrian/M2ChallengeGarrityBrianApplication.java
@@ -0,0 +1,13 @@
+package com.company.M2ChallengeGarrityBrian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2ChallengeGarrityBrianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2ChallengeGarrityBrianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/M2ChallengeGarrityBrian/M2ChallengeGarrityBrianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.M2ChallengeGarrityBrian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2ChallengeGarrityBrianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
