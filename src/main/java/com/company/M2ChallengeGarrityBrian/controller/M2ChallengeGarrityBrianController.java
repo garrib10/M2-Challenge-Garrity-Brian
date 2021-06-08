012f1fd14ec8a3ab2fba6ee9ad77615c17f5111c
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2021-06-07 22:20:53 -0400

    quote works

---
+++ src/main/java/com/company/M2ChallengeGarrityBrian/controller/M2ChallengeGarrityBrianController.java
@@ -0,0 +1,57 @@
+package com.company.M2ChallengeGarrityBrian.controller;
+
+import com.company.M2ChallengeGarrityBrian.models.Answer;
+import com.company.M2ChallengeGarrityBrian.models.Quote;
+import com.company.M2ChallengeGarrityBrian.models.Word;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@RestController
+public class M2ChallengeGarrityBrianController {
+    Random rand = new Random();
+
+    private static List<Quote> quote = new ArrayList<>(Arrays.asList(
+            new Quote(1, "Brian", "I need some coffee")
+    ));
+
+    private static List<Word> word = new ArrayList<>(Arrays.asList(
+            new Word(1, "java", "coffee")
+    ));
+
+    private static List<Answer> answer = new ArrayList<>(Arrays.asList(
+            new Answer(1,"Try Again")
+
+    ));
+
+
+    @RequestMapping(value = "/quote", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public Quote getQuote() {
+
+        int randomQuoteIndex = rand.nextInt(quote.size());
+        Quote selectedQuote = quote.get(randomQuoteIndex);
+        return selectedQuote;
+
+    }
+
+
+//     @RequestMapping(value = "/word", method = RequestMethod.GET)
+//    @ResponseStatus(HttpStatus.OK)
+//   public Word getWord(){
+//
+//     }
+    @RequestMapping(value = "/magic", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Answer getAnswer(@RequestBody String question){
+        int randomAnswerIndex = rand.nextInt(answer.size());
+        Answer selectedAnswer = answer.get(randomAnswerIndex);
+
+        selectedAnswer.setQuestion(question);
+        return selectedAnswer;
+
+     }
+}
---
+++ src/main/java/com/company/M2ChallengeGarrityBrian/models/Word.java
@@ -0,0 +1,53 @@
+package com.company.M2ChallengeGarrityBrian.models;
+
+import java.util.Objects;
+
+public class Word {
+    private int id;
+    private String word;
+    private String definition;
+
+    public Word(int id, String word, String definition) {
+        this.id = id;
+        this.word = word;
+        this.definition = definition;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Word word1 = (Word) o;
+        return id == word1.id && Objects.equals(word, word1.word) && Objects.equals(definition, word1.definition);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, word, definition);
+    }
+
+}
